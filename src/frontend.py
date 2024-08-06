f812891a9832149c9d8a5c757c89310826125f54
ddeiro <ddeiro@bdsdatasolution.com.br>
2024-08-06 09:50:35 -0300

    feat: display result from excel uploaded

--- src/frontend.py
+++ src/frontend.py
@@ -17,3 +17,10 @@ class ExcelValidatorUI:
         
     def upload_file(self):
         return st.file_uploader('Carregue seu arquivo Excel aqui:', type=['xlsx'])
+    
+    def display_results(self, result, errors):
+        if errors:
+            for error in errors:
+                st.error(f'Erro na validação: {error}')
+            else:
+                st.success('O Schema do arquivo Excel está correto!')
